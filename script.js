e4aa7de6242fad98bf549cd2ebeddc06257c8396
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-09-28 21:33:09 -0700

    created passowrd generator

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function writePassword() {
   var lower = confirm("Click OK to confirm inclusion of lowercase characters.");
   var upper = confirm("Click OK to confirm inclusion of uppercase characters.");
 
-  var password = generatePassword();
+  var password = generatePassword(numChar, symbols, number, lower, upper);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
@@ -22,6 +22,21 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  console.log("reach");
+function generatePassword(numChar, symbols, number, lower, upper) {
+  var allowedChars = "";
+  var pw = "";
+
+  if (symbols)
+    allowedChars += " !\"#$ %& '()*+,-./:;<=>?@[\]^_`{|}~";
+  if (number)
+    allowedChars += "0123456789";
+  if (lower)
+    allowedChars += "abcdefghijklmnopqrstuvwxyz";
+  if (upper)
+    allowedChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  for (i = 0; i < numChar; i++)
+    pw += allowedChars.charAt(Math.floor(Math.random() * allowedChars.length));
+
+  return pw;
 }
