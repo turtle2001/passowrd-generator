5f499af12ad5d704678ed26325a3865d5d1e2a8f
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-09-29 00:17:04 -0700

    added comments and fixed edge case

--- script.js
+++ script.js
@@ -3,20 +3,32 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //initial prompt value stored in numChar
   var numChar = prompt("How many characters would you like your password to contain?");
-  while ((numChar < 8 || numChar >= 128) && numChar != undefined) {
-    alert("Password must be between 8 and 100 characters");
+
+  //if cancel is pressed end sequence
+  if (numChar == undefined)
+    return;
+
+  //if value does not meet parameters, repeatedly asks for new value 
+  while (((numChar < 8) || (numChar > 128)) && numChar != undefined) {
+    alert("Password must be between 8 and 128 characters");
     numChar = prompt("How many characters would you like your password to contain?");
+    if (numChar == undefined)
+      return;
   }
 
+  //asks for inclusion of each character type
   var symbols = confirm("Click OK to confirm inclusion of special caracters.");
   var number = confirm("Click OK to confirm inclusion of numeric characters.");
   var lower = confirm("Click OK to confirm inclusion of lowercase characters.");
   var upper = confirm("Click OK to confirm inclusion of uppercase characters.");
 
+  //if no characters are selected dispalys message
   if (!symbols && !number && !lower && !upper)
     alert("Must select at least one character type.")
 
+  //generates passowrd
   var password = generatePassword(numChar, symbols, number, lower, upper);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -26,9 +38,13 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(numChar, symbols, number, lower, upper) {
+  //makes string of allowed characters
   var allowedChars = "";
+
+  //stores password
   var pw = "";
 
+  //adds characters to string if they were included
   if (symbols)
     allowedChars += " !\"#$ %& '()*+,-./:;<=>?@[\]^_`{|}~";
   if (number)
@@ -38,6 +54,7 @@ function generatePassword(numChar, symbols, number, lower, upper) {
   if (upper)
     allowedChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  //chooses random character for  each index of pw
   for (i = 0; i < numChar; i++)
     pw += allowedChars.charAt(Math.floor(Math.random() * allowedChars.length));
 
