82d8dfa0ae99a09f6692971de8d7aac276f5eaa1
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-09-28 21:47:26 -0700

    added no character type selcted clause

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var numChar = prompt("How many characters would you like your password to contain?");
-  while ((numChar < 8 || numChar > 100) && numChar != undefined) {
+  while ((numChar < 8 || numChar >= 128) && numChar != undefined) {
     alert("Password must be between 8 and 100 characters");
     numChar = prompt("How many characters would you like your password to contain?");
   }
@@ -13,6 +13,9 @@ function writePassword() {
   var number = confirm("Click OK to confirm inclusion of numeric characters.");
   var lower = confirm("Click OK to confirm inclusion of lowercase characters.");
   var upper = confirm("Click OK to confirm inclusion of uppercase characters.");
+
+  if (!symbols && !number && !lower && !upper)
+    alert("Must select at least one character type.")
 
   var password = generatePassword(numChar, symbols, number, lower, upper);
   var passwordText = document.querySelector("#password");
